48468ea01645c5c2feaa1f38212ce6533180c0aa
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-22 15:12:27 -0500

    calculadora y entrada de datos

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/01 Introduccion/05.suma.js
@@ -0,0 +1,13 @@
+//Entrada de datos
+let a = prompt("Ingrese un numero");
+let b = prompt("Ingrese otro numero");
+
+//Conversion de datos
+a = Number(a);
+b = Number(b);
+
+//Operacion
+let suma = a+b;
+
+//Salida de datos
+document.write("La suma es: ", suma);
