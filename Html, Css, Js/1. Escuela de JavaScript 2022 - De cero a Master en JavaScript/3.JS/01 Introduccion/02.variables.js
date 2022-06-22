744f043d399b2cc201b5f3fa92f228a48000835b
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-22 14:47:05 -0500

    Tipos de datos en js

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/01 Introduccion/02.variables.js
@@ -0,0 +1,8 @@
+//Definicion de variables
+let nombre ="Jhonny";
+console.log(nombre);
+
+//modificar nombre de la variable
+
+nombre = "otalvaro"
+console.log(nombre);
---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/01 Introduccion/03.tipos de datos.js
@@ -0,0 +1,19 @@
+//Tipo indefinido
+let indefinido;
+console.log(indefinido);
+
+// Tipo de cadena caracteres
+let text = "Hola mundo";
+console.log (typeof(text));
+
+//Tipos de numeros
+let entero = 45;
+console.log(typeof(entero));
+
+let decimal = 4.5;
+console.log(typeof(decimal));
+
+//Tipos Booleanos
+
+console.log(typeof(true))
+console.log(typeof(false));
