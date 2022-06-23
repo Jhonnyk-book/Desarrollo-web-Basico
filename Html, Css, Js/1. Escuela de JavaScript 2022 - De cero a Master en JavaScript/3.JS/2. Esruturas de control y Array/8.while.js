846500de8e23614b6b64f53d0c684416e5069fbb
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-23 16:07:56 -0500

    sentencia while y do while en js

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/2. Esruturas de control y Array/8.while.js
@@ -0,0 +1,26 @@
+let n = 2, c = 0;
+
+while(c <= 10){
+        console.log(`${n} x ${c} = ${n * c}`);
+        c++;
+}
+
+let texts = 'javascript';
+let estudiantes= ['alex', 'jhonny', 'kira', 'david'];
+
+console.log('--------------------------------------------------------');
+
+let i = 0;
+
+while(i < estudiantes.length){
+    console.log(estudiantes[i]);
+    i++;
+}
+
+console.log('--------------------------------------------------------');
+
+let s = 0;
+do{
+    console.log('Hola');
+    s++;
+}while(s < 5);
