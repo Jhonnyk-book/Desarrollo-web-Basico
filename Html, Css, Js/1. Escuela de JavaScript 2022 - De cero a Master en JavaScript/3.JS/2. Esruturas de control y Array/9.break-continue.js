4714fc9e553700f0471fd29672f5d91331c626b9
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-23 16:16:43 -0500

    break y continue en js

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/2. Esruturas de control y Array/9.break-continue.js
@@ -0,0 +1,12 @@
+let c = 0;
+
+while(c <= 10){
+    c++;
+    if(c === 5){
+        ///console.log("El bucle termina en 5");
+        ///break;
+        console.log("El bcle continua despues de 5");
+        continue;
+    }
+    console.log(c);
+}
