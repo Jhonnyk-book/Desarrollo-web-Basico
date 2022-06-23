c4d1d4148c3b79e314c4e31583b8376b4b8d76ac
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-22 23:07:45 -0500

    Conicionales en js

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/2. Esruturas de control y Array/1.condicionales.js
@@ -0,0 +1,18 @@
+/*
+if(false){
+    console.log("Se cumple la condicion");
+}
+else{
+    console.log("No se cumple la condicion");
+}
+*/
+
+let n = 5, print ="";
+ if(n % 2 == 0){
+    print(`El numero ${n} es par`);
+ }
+ else{
+    print(`El numero ${n} es impar`);
+ }
+
+ console.log(print);
