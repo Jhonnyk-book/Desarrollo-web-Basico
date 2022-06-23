67eb22c2e5045af72be4572bcf8675d24357f264
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-23 13:12:19 -0500

    Uso de Switch

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/2. Esruturas de control y Array/4.switch.js
@@ -0,0 +1,20 @@
+let v = 'z', print = '';
+
+switch(v){
+    case 'a':
+    case 'A': 
+    case 'e': 
+    case 'E': 
+    case 'i': 
+    case 'I': 
+    case 'o': 
+    case 'O': 
+    case 'u':
+    case 'U':
+        print = `${v} es voval`;
+        break;
+    default:
+        print = `${v} no es vocal`
+        break;
+}
+console.log(print);
