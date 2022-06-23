b170aa6f53081f8abc3ddff26318cb32f8802faf
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-23 18:48:22 -0500

    Parametros y argumentos en js

---
+++ Html, Css, Js/1. Escuela de JavaScript 2022 - De cero a Master en JavaScript/3.JS/3. funciones/2.Parametros y argumentos.js
@@ -0,0 +1,10 @@
+//donde desta nombre es un parametro
+//Los parametros estan en la funion
+//El parametro es el que recibe
+function saludar(nombre){
+    console.log(`Hola, ${nombre} desde una funcion`);
+}
+
+//Los argumentos estan en la llamada de la funcion
+//Los argumentos son cuando envias la informacion
+saludar('Kira');
