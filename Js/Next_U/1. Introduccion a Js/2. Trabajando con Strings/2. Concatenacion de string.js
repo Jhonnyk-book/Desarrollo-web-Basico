7a97b690b4582b973b57efb14618b19df59c3660
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-28 18:29:00 -0500

    concatenacion de string en js

---
+++ Js/Next_U/1. Introduccion a Js/2. Trabajando con Strings/2. Concatenacion de string.js
@@ -0,0 +1,6 @@
+function saludar(nombre, apellido, curso){
+    let resultado = `Hola ${nombre} ${apellido}, bienvenido al curso ${curso}`;
+    return resultado;
+}
+
+console.log(saludar("Kira", "Negra", "C++"));
