64c9463d51df1500a17e6e620f63f3e7d09cf2b6
Jhonnyer Otalvaro <jhonnyer.o95@gmail.com>
2022-06-29 19:56:29 -0500

    Condicionales en js

---
+++ Js/Next_U/1. Introduccion a Js/3. Funciones y condicionales/1.Trabajando con booleadnos y condicionales.js
@@ -0,0 +1,12 @@
+let tieneCocina = false;
+let tieneWifi = true;
+let maximaOcupacion = 4;
+let lugar = "Ciudad";
+
+if (lugar == "Playa"){
+    console.log("Hotel A1");
+} else if( lugar == "Lago"){
+    console.log("Hotel B2");
+} else{
+    console.log("Hotel C3");
+}
